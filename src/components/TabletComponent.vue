0a3e77afccc3befb63d777463a58cdf1c8276368
nionoku <nionoku@gmail.com>
2020-03-25 18:12:24 +0300

    Coloring cells

--- src/components/TabletComponent.vue
+++ src/components/TabletComponent.vue
@@ -5,12 +5,12 @@
       <div
         v-for="(j) in (tablet.length + 1 - i)"
         :key="[1, i, j].join('')"
+        :class="`item color-row-${i} color-col-${j + 1}`"
         :style="{ 'grid-row': i, 'grid-column': j + 1 }"
         @click="setPotition(tablet.length - i, j - 1)"
       >
         <img
           :src="potions[tablet[tablet.length - i][j - 1]]"
-          height="10px"
           alt=""
         >
       </div>
@@ -19,12 +19,12 @@
       <div
         v-for="(j) in (tablet.length + 1 - i)"
         :key="[2, i, j].join('')"
+        :class="`item color-row-${cells.size - j} color-col-${cells.size - i + 1}`"
         :style="{ 'grid-row': cells.size - j, 'grid-column': cells.size + 1 - i }"
         @click="setPotition(tablet.length - i, j - 1)"
       >
         <img
           :src="potions[tablet[tablet.length - i][j - 1]]"
-          height="10px"
           alt=""
         >
       </div>
@@ -34,36 +34,47 @@
     <template v-for="(i) in cells.size">
       <div
         :key="[3, i].join('')"
+        :class="`gradient-${i}`"
         :style="{ 'grid-row': i, 'grid-column': cells.size + 1 - i }"
-      >
-        X
-      </div>
+      />
     </template>
 
     <!-- Template for left column ingridients -->
     <template v-for="(i) in ingridients.length">
       <div
         :key="[4, i].join('')"
+        :class="`item color-row-${i}`"
         :style="{ 'grid-row': i, 'grid-column': 1 }"
       >
         <img
           :src="ingridients[ingridients.length - i]"
-          height="10px"
           alt=""
         >
       </div>
 
       <div
         :key="[5, i].join('')"
+        :class="`item color-col-${i + 1}`"
         :style="{ 'grid-row': cells.size, 'grid-column': i + 1 }"
       >
         <img
           :src="ingridients[i - 1]"
-          height="10px"
           alt=""
         >
       </div>
     </template>
+
+    <svg
+      viewBox="0 0 100 100"
+      preserveAspectRatio="none"
+    >
+      <line
+        x1="0"
+        y1="100%"
+        x2="100%"
+        y2="0"
+      />
+    </svg>
   </div>
 </template>
 
@@ -71,10 +82,167 @@
 .tablet-grid {
   width: 100%;
   height: 100%;
+  background: transparent;
+  position: relative;
 
   display: grid;
+  grid-gap: 1vh;
   grid-template-columns: repeat(9, 1fr);
   grid-template-rows: repeat(9, 1fr);
+
+  .item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      max-width: 100%;
+      max-height: 100%;
+
+      opacity: 0.1;
+    }
+  }
+
+  svg {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    stroke-dasharray: 1;
+    stroke-width: 0.1;
+    // stroke: rgb(180, 180, 180);
+    stroke: rgb(0, 0, 0);
+  }
+}
+
+.color-col-2,
+.color-row-8 {
+  background-color: var(--color-mushroom);
+}
+
+.color-col-3,
+.color-row-7 {
+  background-color: var(--color-ivy);
+}
+
+.color-col-4,
+.color-row-6 {
+  background-color: var(--color-toad);
+}
+
+.color-col-5,
+.color-row-5 {
+  background-color: var(--color-chicken-foot);
+}
+
+.color-col-6,
+.color-row-4 {
+  background-color: var(--color-flower);
+}
+
+.color-col-7,
+.color-row-3 {
+  background-color: var(--color-mandrake);
+}
+
+.color-col-8,
+.color-row-2 {
+  background-color: var(--color-scorpion);
+}
+
+.color-col-9,
+.color-row-1 {
+  background-color: var(--color-crow-feather);
+}
+
+.gradient-9 {
+  background: linear-gradient(
+    to top right,
+    transparent 0,
+    transparent 30%,
+    var(--color-mushroom) 65%,
+    var(--color-mushroom) 100%
+  );
+}
+
+.gradient-8 {
+  background: linear-gradient(
+    to top right,
+    var(--color-mushroom) 0,
+    var(--color-mushroom) 40%,
+    var(--color-ivy) 60%,
+    var(--color-ivy) 100%
+  );
+}
+
+.gradient-7 {
+  background: linear-gradient(
+    to top right,
+    var(--color-ivy) 0,
+    var(--color-ivy) 40%,
+    var(--color-toad) 70%,
+    var(--color-toad) 100%
+  );
+}
+
+.gradient-6 {
+  background: linear-gradient(
+    to top right,
+    var(--color-toad) 0,
+    var(--color-toad) 30%,
+    var(--color-chicken-foot) 70%,
+    var(--color-chicken-foot) 100%
+  );
+}
+
+.gradient-5 {
+  background: linear-gradient(
+    to top right,
+    var(--color-chicken-foot) 0,
+    var(--color-chicken-foot) 30%,
+    var(--color-flower) 70%,
+    var(--color-flower) 100%
+  );
+}
+
+.gradient-4 {
+  background: linear-gradient(
+    to top right,
+    var(--color-flower) 0,
+    var(--color-flower) 30%,
+    var(--color-mandrake) 63%,
+    var(--color-mandrake) 100%
+  );
+}
+
+.gradient-3 {
+  background: linear-gradient(
+    to top right,
+    var(--color-mandrake) 0,
+    var(--color-mandrake) 40%,
+    var(--color-scorpion) 70%,
+    var(--color-scorpion) 100%
+  );
+}
+
+.gradient-2 {
+  background: linear-gradient(
+    to top right,
+    var(--color-scorpion) 0,
+    var(--color-scorpion) 30%,
+    var(--color-crow-feather) 63%,
+    var(--color-crow-feather) 100%
+  );
+}
+
+.gradient-1 {
+  background: linear-gradient(
+    to top right,
+    var(--color-crow-feather) 0,
+    var(--color-crow-feather) 30%,
+    transparent 70%,
+    transparent 100%
+  );
 }
 </style>
 
