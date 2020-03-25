53e49147cc91457cb7ee8cfe4790199069e93bda
Alexander <kondrev@trinity.local>
2020-03-25 17:18:11 +0300

    Add potitions and ingridients arrays

--- src/view-models/TabletComponent.ts
+++ src/view-models/TabletComponent.ts
@@ -4,7 +4,7 @@ import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
 export default class TabletComponent extends Vue {
   @Prop({ required: true })
   protected value!: Array<Array<number>>
-  protected tablet = [
+  protected readonly sTablet = [
     [0],
     [0, 0],
     [0, 0, 0],
@@ -13,8 +13,46 @@ export default class TabletComponent extends Vue {
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0]
   ];
+  protected readonly cells = {
+    size: 9
+  };
 
-  @Watch('tablet')
+  protected get tablet () {
+    return this.sTablet;
+  }
+  protected get potions () {
+    return [
+      'img/game/potion_neutral.png',
+      'img/game/potion_blue_positive.png',
+      'img/game/potion_blue_negative.png',
+      'img/game/potion_green_positive.png',
+      'img/game/potion_green_negative.png',
+      'img/game/potion_red_positive.png',
+      'img/game/potion_red_negative.png'
+    ];
+  }
+  protected get ingridients () {
+    return [
+      'img/game/mushroom.jpg',
+      'img/game/ivy.jpg',
+      'img/game/toad.jpg',
+      'img/game/chicken-foot.jpg',
+      'img/game/flower.jpg',
+      'img/game/mandrake.jpg',
+      'img/game/scorpion.jpg',
+      'img/game/crow-feather.jpg'
+    ];
+  }
+
+  protected setPotition (i: number, j: number) {
+    this.$set(this.sTablet[i], j, this.sTablet[i][j] + 1);
+
+    if (this.sTablet[i][j] >= this.potions.length) {
+      this.$set(this.sTablet[i], j, 0);
+    }
+  }
+
+  @Watch('sTablet')
   protected onTabletHasChanged (value: Array<Array<number>>) {
     this.$emit('input', value);
   }
