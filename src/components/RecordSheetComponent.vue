057cd238624720ce4f83da662b3d8f0ed8635cac
Alexander <kondrev@trinity.local>
2020-03-26 16:41:37 +0300

    Add template of records sheet

--- src/components/RecordSheetComponent.vue
+++ src/components/RecordSheetComponent.vue
@@ -1,5 +1,106 @@
 <template>
-  <div>Hello world record sheet</div>
+  <div class="record-sheet-grid">
+    <template v-for="(item_i, i) in recordSheet.length">
+      <div
+        v-for="(item_j, j) in recordSheet[i].length"
+        :key="[7, i, j].join('')"
+        :class="`item item-col item-row color-col-${j + 1} color-row-${i + 1}`"
+      >
+        X
+      </div>
+    </template>
+
+    <template v-for="(i) in formulas.length">
+      <!-- Template for left column formulas -->
+      <div
+        :key="[4, i].join('')"
+        :class="`item item-row ingridient color-row-${i}`"
+        :style="{ 'grid-row': i, 'grid-column': 1 }"
+      >
+        <img
+          :src="formulas[formulas.length - i]"
+          alt=""
+        >
+      </div>
+
+      <!-- Template for bottom row ingridients -->
+      <div
+        :key="[5, i].join('')"
+        :class="`item item-col ingridient color-col-${i}`"
+        :style="{ 'grid-row': cells.size, 'grid-column': i + 1 }"
+      >
+        <img
+          :src="ingridients[i - 1]"
+          alt=""
+        >
+      </div>
+    </template>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.record-sheet-grid {
+  width: 100%;
+  height: 100%;
+
+  display: grid;
+  grid-template-columns: repeat(9, 1fr);
+  grid-template-rows: repeat(9, 1fr);
+
+  .item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1vh;
+
+    img {
+      max-width: 100%;
+      max-height: 100%;
+
+      opacity: var(--opacity-images);
+    }
+  }
+}
+
+.color-col-1 {
+  background-color: var(--color-mushroom);
+}
+
+.color-col-2 {
+  background-color: var(--color-ivy);
+}
+
+.color-col-3 {
+  background-color: var(--color-toad);
+}
+
+.color-col-4 {
+  background-color: var(--color-chicken-foot);
+}
+
+.color-col-5 {
+  background-color: var(--color-flower);
+}
+
+.color-col-6 {
+  background-color: var(--color-mandrake);
+}
+
+.color-col-7 {
+  background-color: var(--color-scorpion);
+}
+
+.color-col-8 {
+  background-color: var(--color-crow-feather);
+}
+
+.item-col {
+  border-left: 1px solid var(--color-dilimiter-line);
+}
+
+.item-row {
+  border-bottom: 1px solid var(--color-dilimiter-line);
+}
+</style>
 
 <script lang="ts" src="@/view-models/RecordSheetComponent" />
--- src/view-models/RecordSheetComponent.ts
+++ src/view-models/RecordSheetComponent.ts
@@ -2,4 +2,42 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class RecordSheetComponent extends Vue {
+  protected readonly cells = {
+    size: 9
+  };
+  protected recordSheet = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+
+  protected get ingridients () {
+    return [
+      'img/game/mushroom.jpg',
+      'img/game/ivy.jpg',
+      'img/game/toad.jpg',
+      'img/game/chicken-foot.jpg',
+      'img/game/flower.jpg',
+      'img/game/mandrake.jpg',
+      'img/game/scorpion.jpg',
+      'img/game/crow-feather.jpg'
+    ];
+  }
+  protected get formulas () {
+    return [
+      'img/game/rln_gln_bln.png',
+      'img/game/rln_gsn_bsp.png',
+      'img/game/rlp_glp_blp.png',
+      'img/game/rlp_gsp_bsn.png',
+      'img/game/rsn_glp_bsp.png',
+      'img/game/rsn_gsp_bln.png',
+      'img/game/rsp_gln_bsn.png',
+      'img/game/rsp_gsn_blp.png'
+    ];
+  }
 }
