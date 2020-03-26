8be2f1d1105d3fa2fae7270ec477dd351d1d1048
nionoku <nionoku@gmail.com>
2020-03-26 22:59:53 +0300

    Add calculating for formulas by potions

--- src/view-models/RecordSheetComponent.ts
+++ src/view-models/RecordSheetComponent.ts
@@ -1,5 +1,7 @@
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+// eslint-disable-next-line no-unused-vars
 import { ItemModel, ItemSize, ItemValue } from '@/models/ElementModel';
+import { RecordSheetItem } from '@/utils/RecordSheetItem';
 
 @Component
 export default class RecordSheetComponent extends Vue {
@@ -8,15 +10,15 @@ export default class RecordSheetComponent extends Vue {
   protected readonly cells = {
     size: 9
   };
-  protected readonly recordSheet = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0]
+  protected readonly recordSheet: Array<Array<RecordSheetItem>> = [
+    [RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY],
+    [RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY],
+    [RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY],
+    [RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY],
+    [RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY],
+    [RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY],
+    [RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY],
+    [RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY, RecordSheetItem.EMPTY]
   ];
   protected readonly formulasValues: Array<Array<ItemModel>> = [
     [
@@ -164,32 +166,44 @@ export default class RecordSheetComponent extends Vue {
 
   @Watch('value')
   protected onValueUpdated (value: Array<Array<Array<number>>>) {
+    for (let i = 0; i < this.recordSheet.length; i++) {
+      for (let j = 0; j < this.recordSheet[i].length; j++) {
+        this.$set(this.recordSheet[i], j, RecordSheetItem.EMPTY);
+      }
+    }
+
     for (let i = 0; i < value.length; i++) {
       for (let j = 0; j < value[i].length; j++) {
 
-        // for 1 color potions
-        if (value[i][j].filter(it => it != 0).length == 1) {
-          // TODO
+        // for soup
+        if (value[i][j].every(it => it == ItemValue.NEUTRAL)) {
+          for (let k = 0; k < this.recordSheet[i].length; k++) {
+            if (this.recordSheet[i + 1][k] != RecordSheetItem.EMPTY) {
+              this.$set(this.recordSheet[j], k % 2 > 0 ? k - 1 : k + 1, RecordSheetItem.MARK);
+            }
 
+            if (this.recordSheet[j][k] != RecordSheetItem.EMPTY) {
+              this.$set(this.recordSheet[i + 1], k % 2 > 0 ? k - 1 : k + 1, RecordSheetItem.MARK);
+            }
+          }
+          // for 1 color potions
+        } else if (value[i][j].filter(it => it != ItemValue.NEUTRAL).length == 1) {
+          for (let k = 0; k < this.recordSheet[i].length; k++) {
+            if (value[i][j].some((it, index) => it != ItemValue.NEUTRAL && it != this.formulasValues[k][index].value)) {
+              this.$set(this.recordSheet[i + 1], k, RecordSheetItem.MARK);
+              this.$set(this.recordSheet[j], k, RecordSheetItem.MARK);
+            }
+          }
           // for 2 color's potions
-        } else if (value[i][j].filter(it => it != 0).length == 2) {
-          // TODO
-        } else {
-
-        }
-
-
-        for (let k = 0; k < this.recordSheet[i].length; k++) {
-
-          if (this.formulasValues[k].blue.value == ItemValue.POSITIVE) {
-            // i + 1 = coord by x
-            // j = coord by y
-            this.$set(this.recordSheet[i + 1], k, -1);
-            this.$set(this.recordSheet[j], k, -1);
+        } else if (value[i][j].filter(it => it != ItemValue.NEUTRAL).length == 2) {
+          for (let k = 0; k < this.recordSheet[i].length; k++) {
+            if (value[i][j].every((it, index) => it != this.formulasValues[k][index].value)) {
+              this.$set(this.recordSheet[i + 1], k, RecordSheetItem.MARK);
+              this.$set(this.recordSheet[j], k, RecordSheetItem.MARK);
+            }
           }
         }
       }
     }
   }
-}
 }
