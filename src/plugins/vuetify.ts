3401895704e4b9ca3dfc42fba40152bafa33961f
Alexander Kondrev <kondrev@trinity.local>
2020-03-27 12:18:38 +0300

    Recolor tabs

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -14,7 +14,7 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: '#ee44aa',
+        primary: '#d4ba89',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
